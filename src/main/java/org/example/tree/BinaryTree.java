1f8427e7d86fb14a4cf9036f9e0920c1e877434a
stephen <isaacsteverado9@gmail.com>
2026-02-25 02:29:08 +0100

    learned more on binary tree

--- src/main/java/org/example/tree/BinaryTree.java
+++ src/main/java/org/example/tree/BinaryTree.java
@@ -16,8 +16,28 @@ public class BinaryTree {
     Node root;
 
     public void insert(int data) {
+        root = insertRec(root, data);
+    }
 
+    public Node insertRec(Node root, int data) {
         if(root == null)
             root = new Node(data);
+        else if(data < root.data)
+            root.left = insertRec(root.left, data);
+        else if(data > root.data)
+            root.right = insertRec(root.right, data);
+
+        return root;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+    }
+    public void inorderRec(Node root) {
+        if(root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
     }
 }
--- src/main/java/org/example/tree/Main.java
+++ src/main/java/org/example/tree/Main.java
@@ -7,6 +7,12 @@ public class Main {
         BinaryTree tree = new BinaryTree();
         tree.insert(8);
         tree.insert(7);
+        tree.insert(12);
+        tree.insert(15);
+        tree.insert(2);
+        tree.insert(5);
+
+        tree.inorder();
 
     }
 }
